a69c11d8cc08c13c625562f7ab48274feaa6fab8
Evan Gray <evan.gray@rackspace.com>
2014-06-22 17:46:08 -0500

    commit message yeah cool

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ var express = require('express');
 var app = express();
 var jade = require('jade');
 
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-    console.log(req);
     res.render('home.jade');
 });
 
